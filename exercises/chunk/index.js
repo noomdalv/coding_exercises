68572924e595af092c3b4cefb4dab2c39ba32588
noomdalv <vladlunasan@gmail.com>
2020-06-20 01:01:58 -0500

    added 2 extra solutions for array split exercise

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -27,4 +27,34 @@ function chunk(array, size) {
   return newArr;
 }
 
+// function chunk(array, size) {
+// 	let chunked = [];
+//
+// 	for (let element of array) {
+// 		const last = chunked[chunked.length - 1];
+//
+// 		if (!last || last.length === size) {
+// 			chunked.push([element])
+// 		} else {
+// 			last.push(element);
+// 		}
+//
+// 		return chunked;
+// 	}
+// }
+
+function chunk(array, size) {
+	let chunked = [];
+	let index = 0;
+
+	while (index < array.length) {
+		chunked.push(array.slice(index, index + size));
+		index += size;
+	}
+
+	return chunked;
+}
+
+
+
 module.exports = chunk;
